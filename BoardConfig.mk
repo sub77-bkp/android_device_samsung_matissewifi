549f67c82e1fbc1810539c1fca045db5e0dc4690
sub77 <sub77@ymail.com>
2015-06-08 03:32:06 +0200

    TC_ROM 4.9-sm / Strict

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,11 +56,11 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
 # TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/matissewifi
 
 # BlissPop Configs
-TARGET_TC_ROM := 4.8-sm
+TARGET_TC_ROM := 4.9-sm
 TARGET_TC_KERNEL := 4.9-sm
 BLISSIFY := true
 BLISS_O3 := true
-BLISS_STRICT := false
+BLISS_STRICT := true
 BLISS_GRAPHITE := true
 BLISS_KRAIT := true
 BLISS_PIPE := true
