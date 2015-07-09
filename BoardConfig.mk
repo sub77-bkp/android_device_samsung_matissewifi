479a00401c0afdd008eec0ace7113ffd0e69016d
sub77 <sub77@ymail.com>
2015-07-09 15:07:03 +0200

    selinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,28 +23,38 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 # Kernel
 TARGET_PREBUILT_KERNEL := device/samsung/matissewifi/kernel
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/matissewifi/custombootimg.mk
-#BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 androidboot.bootdevice=msm_sdcc.1
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/matissewifi/mkbootimg.mk
+TARGET_KERNEL_SOURCE := kernel/samsung/matissewifi
+TARGET_KERNEL_CONFIG := matissewifi_defconfig
+#TARGET_KERNEL_VARIANT_CONFIG := msm8226-sec_matissewifi_defconfig
+#TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig twrp-sec_defconfig
+BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 androidboot.bootdevice=msm_sdcc.1
 #BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37
-#BOARD_KERNEL_BASE := 0x00000000
-#BOARD_KERNEL_PAGESIZE := 2048
-#BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 #BOARD_MKBOOTIMG_ARGS := --dt device/samsung/matissewifi/dt.img --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 
 # USB Mounting
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
+# TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
 
 # TWRP-Specific
-TW_THEME := landscape_mdpi
+TW_THEME := landscape_hdpi
+TW_NEW_ION_HEAP := true
 HAVE_SELINUX := true
 RECOVERY_SDCARD_ON_DATA := true
-TW_INCLUDE_CRYPTO := true
+TW_FLASH_FROM_STORAGE := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_NO_USB_STORAGE := true
+#TW_INCLUDE_CRYPTO := true
 TW_INTERNAL_STORAGE_PATH := "/data/media/0"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
@@ -56,7 +66,7 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 
 MR_INPUT_TYPE := type_b
 MR_INIT_DEVICES := device/samsung/matissewifi/multirom/mr_init_devices.c
-MR_DPI := mhdpi
+MR_DPI := hdpi
 MR_DPI_MUL := 1
 MR_DPI_FONT := 160
 MR_FSTAB := device/samsung/matissewifi/recovery.fstab
